92e8a3127bdce0ea1b7cc12d7b9b2169443c0c8a
jeremyqzt <jq357@hotmail.com>
2015-11-07 13:54:06 -0500

    pin part

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -22,32 +22,232 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.PassKey1 = New MaterialSkin.Controls.MaterialRaisedButton()
         Me.MaterialRaisedButton1 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton2 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton3 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton4 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton5 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton6 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton7 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton8 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.PassField = New MaterialSkin.Controls.MaterialSingleLineTextField()
+        Me.MaterialRaisedButton9 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton10 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialRaisedButton11 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
         Me.SuspendLayout()
+        '
+        'PassKey1
+        '
+        Me.PassKey1.Depth = 0
+        Me.PassKey1.Location = New System.Drawing.Point(204, 217)
+        Me.PassKey1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.PassKey1.Name = "PassKey1"
+        Me.PassKey1.Primary = True
+        Me.PassKey1.Size = New System.Drawing.Size(72, 68)
+        Me.PassKey1.TabIndex = 0
+        Me.PassKey1.Text = "1"
+        Me.PassKey1.UseVisualStyleBackColor = True
         '
         'MaterialRaisedButton1
         '
         Me.MaterialRaisedButton1.Depth = 0
-        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(185, 156)
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(304, 217)
         Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
         Me.MaterialRaisedButton1.Primary = True
-        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(479, 141)
-        Me.MaterialRaisedButton1.TabIndex = 0
-        Me.MaterialRaisedButton1.Text = "MaterialRaisedButton1"
+        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton1.TabIndex = 1
+        Me.MaterialRaisedButton1.Text = "2"
         Me.MaterialRaisedButton1.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton2
+        '
+        Me.MaterialRaisedButton2.Depth = 0
+        Me.MaterialRaisedButton2.Location = New System.Drawing.Point(403, 217)
+        Me.MaterialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton2.Name = "MaterialRaisedButton2"
+        Me.MaterialRaisedButton2.Primary = True
+        Me.MaterialRaisedButton2.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton2.TabIndex = 2
+        Me.MaterialRaisedButton2.Text = "3"
+        Me.MaterialRaisedButton2.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton3
+        '
+        Me.MaterialRaisedButton3.Depth = 0
+        Me.MaterialRaisedButton3.Location = New System.Drawing.Point(204, 306)
+        Me.MaterialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton3.Name = "MaterialRaisedButton3"
+        Me.MaterialRaisedButton3.Primary = True
+        Me.MaterialRaisedButton3.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton3.TabIndex = 3
+        Me.MaterialRaisedButton3.Text = "4"
+        Me.MaterialRaisedButton3.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton4
+        '
+        Me.MaterialRaisedButton4.Depth = 0
+        Me.MaterialRaisedButton4.Location = New System.Drawing.Point(304, 306)
+        Me.MaterialRaisedButton4.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton4.Name = "MaterialRaisedButton4"
+        Me.MaterialRaisedButton4.Primary = True
+        Me.MaterialRaisedButton4.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton4.TabIndex = 4
+        Me.MaterialRaisedButton4.Text = "5"
+        Me.MaterialRaisedButton4.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton5
+        '
+        Me.MaterialRaisedButton5.Depth = 0
+        Me.MaterialRaisedButton5.Location = New System.Drawing.Point(403, 306)
+        Me.MaterialRaisedButton5.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton5.Name = "MaterialRaisedButton5"
+        Me.MaterialRaisedButton5.Primary = True
+        Me.MaterialRaisedButton5.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton5.TabIndex = 5
+        Me.MaterialRaisedButton5.Text = "6"
+        Me.MaterialRaisedButton5.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton6
+        '
+        Me.MaterialRaisedButton6.Depth = 0
+        Me.MaterialRaisedButton6.Location = New System.Drawing.Point(204, 398)
+        Me.MaterialRaisedButton6.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton6.Name = "MaterialRaisedButton6"
+        Me.MaterialRaisedButton6.Primary = True
+        Me.MaterialRaisedButton6.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton6.TabIndex = 6
+        Me.MaterialRaisedButton6.Text = "7"
+        Me.MaterialRaisedButton6.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton7
+        '
+        Me.MaterialRaisedButton7.Depth = 0
+        Me.MaterialRaisedButton7.Location = New System.Drawing.Point(304, 398)
+        Me.MaterialRaisedButton7.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton7.Name = "MaterialRaisedButton7"
+        Me.MaterialRaisedButton7.Primary = True
+        Me.MaterialRaisedButton7.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton7.TabIndex = 7
+        Me.MaterialRaisedButton7.Text = "8"
+        Me.MaterialRaisedButton7.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton8
+        '
+        Me.MaterialRaisedButton8.Depth = 0
+        Me.MaterialRaisedButton8.Location = New System.Drawing.Point(403, 398)
+        Me.MaterialRaisedButton8.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton8.Name = "MaterialRaisedButton8"
+        Me.MaterialRaisedButton8.Primary = True
+        Me.MaterialRaisedButton8.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton8.TabIndex = 8
+        Me.MaterialRaisedButton8.Text = "9"
+        Me.MaterialRaisedButton8.UseVisualStyleBackColor = True
+        '
+        'PassField
+        '
+        Me.PassField.Depth = 0
+        Me.PassField.Hint = ""
+        Me.PassField.Location = New System.Drawing.Point(204, 158)
+        Me.PassField.MouseState = MaterialSkin.MouseState.HOVER
+        Me.PassField.Name = "PassField"
+        Me.PassField.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.PassField.SelectedText = ""
+        Me.PassField.SelectionLength = 0
+        Me.PassField.SelectionStart = 0
+        Me.PassField.Size = New System.Drawing.Size(271, 23)
+        Me.PassField.TabIndex = 9
+        Me.PassField.UseSystemPasswordChar = False
+        '
+        'MaterialRaisedButton9
+        '
+        Me.MaterialRaisedButton9.Depth = 0
+        Me.MaterialRaisedButton9.Location = New System.Drawing.Point(304, 484)
+        Me.MaterialRaisedButton9.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton9.Name = "MaterialRaisedButton9"
+        Me.MaterialRaisedButton9.Primary = True
+        Me.MaterialRaisedButton9.Size = New System.Drawing.Size(171, 68)
+        Me.MaterialRaisedButton9.TabIndex = 10
+        Me.MaterialRaisedButton9.Text = "Enter"
+        Me.MaterialRaisedButton9.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton10
+        '
+        Me.MaterialRaisedButton10.Depth = 0
+        Me.MaterialRaisedButton10.Location = New System.Drawing.Point(204, 484)
+        Me.MaterialRaisedButton10.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton10.Name = "MaterialRaisedButton10"
+        Me.MaterialRaisedButton10.Primary = True
+        Me.MaterialRaisedButton10.Size = New System.Drawing.Size(72, 68)
+        Me.MaterialRaisedButton10.TabIndex = 11
+        Me.MaterialRaisedButton10.Text = "<-"
+        Me.MaterialRaisedButton10.UseVisualStyleBackColor = True
+        '
+        'MaterialRaisedButton11
+        '
+        Me.MaterialRaisedButton11.Depth = 0
+        Me.MaterialRaisedButton11.Location = New System.Drawing.Point(527, 73)
+        Me.MaterialRaisedButton11.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton11.Name = "MaterialRaisedButton11"
+        Me.MaterialRaisedButton11.Primary = True
+        Me.MaterialRaisedButton11.Size = New System.Drawing.Size(172, 68)
+        Me.MaterialRaisedButton11.TabIndex = 12
+        Me.MaterialRaisedButton11.Text = "Cancel"
+        Me.MaterialRaisedButton11.UseVisualStyleBackColor = True
+        '
+        'MaterialLabel1
+        '
+        Me.MaterialLabel1.AutoSize = True
+        Me.MaterialLabel1.Depth = 0
+        Me.MaterialLabel1.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.MaterialLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialLabel1.Location = New System.Drawing.Point(200, 112)
+        Me.MaterialLabel1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialLabel1.Name = "MaterialLabel1"
+        Me.MaterialLabel1.Size = New System.Drawing.Size(153, 19)
+        Me.MaterialLabel1.TabIndex = 13
+        Me.MaterialLabel1.Text = "Please Enter Your Pin"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1403, 644)
+        Me.ClientSize = New System.Drawing.Size(711, 599)
+        Me.Controls.Add(Me.MaterialLabel1)
+        Me.Controls.Add(Me.MaterialRaisedButton11)
+        Me.Controls.Add(Me.MaterialRaisedButton10)
+        Me.Controls.Add(Me.MaterialRaisedButton9)
+        Me.Controls.Add(Me.PassField)
+        Me.Controls.Add(Me.MaterialRaisedButton8)
+        Me.Controls.Add(Me.MaterialRaisedButton7)
+        Me.Controls.Add(Me.MaterialRaisedButton6)
+        Me.Controls.Add(Me.MaterialRaisedButton5)
+        Me.Controls.Add(Me.MaterialRaisedButton4)
+        Me.Controls.Add(Me.MaterialRaisedButton3)
+        Me.Controls.Add(Me.MaterialRaisedButton2)
         Me.Controls.Add(Me.MaterialRaisedButton1)
+        Me.Controls.Add(Me.PassKey1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents PassKey1 As MaterialSkin.Controls.MaterialRaisedButton
     Friend WithEvents MaterialRaisedButton1 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton2 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton3 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton4 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton5 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton6 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton7 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton8 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents PassField As MaterialSkin.Controls.MaterialSingleLineTextField
+    Friend WithEvents MaterialRaisedButton9 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton10 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialRaisedButton11 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialLabel1 As MaterialSkin.Controls.MaterialLabel
 End Class
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -9,7 +9,65 @@ Public Class Form1
         SkinManager.ColorScheme = New ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE)
     End Sub
 
-    Private Sub MaterialRaisedButton1_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton1.Click
-        MsgBox("hi")
+    Private Sub MaterialRaisedButton1_Click(sender As Object, e As EventArgs) Handles PassKey1.Click
+        Call AddStar()
+    End Sub
+
+    Private Sub MaterialRaisedButton1_Click_1(sender As Object, e As EventArgs) Handles MaterialRaisedButton1.Click
+        Call AddStar()
+    End Sub
+    Public Function AddStar() As Integer
+        Dim current As String
+        current = PassField.Text
+        current = current + "   *"
+        PassField.Text = current
+        Return 0
+    End Function
+
+    Private Sub MaterialRaisedButton2_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton2.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton3_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton3.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton4_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton4.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton5_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton5.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton6_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton6.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton7_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton7.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton8_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton8.Click
+        Call AddStar()
+
+    End Sub
+
+    Private Sub MaterialRaisedButton10_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton10.Click
+        Dim Removed As String
+        Dim Amount As Integer
+        Removed = PassField.Text
+        Amount = Removed.Length
+        If Amount > 3 Then
+            PassField.Text = Removed.Substring(0, Amount - 4)
+        Else
+
+        End If
     End Sub
 End Class
