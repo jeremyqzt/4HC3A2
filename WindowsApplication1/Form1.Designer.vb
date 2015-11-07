7df0e6b8b9f8b029d66aebd208889b99e2f74cea
IanYXXL <ianyang12345@hotmail.com>
2015-11-07 13:43:40 -0500

    Testing VB

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -23,31 +23,80 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.MaterialRaisedButton1 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.ToMain = New System.Windows.Forms.Button()
+        Me.Withdraw = New System.Windows.Forms.Button()
+        Me.Deposit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'MaterialRaisedButton1
         '
         Me.MaterialRaisedButton1.Depth = 0
-        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(185, 156)
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(900, 32)
         Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
         Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
         Me.MaterialRaisedButton1.Primary = True
-        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(479, 141)
+        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(29, 27)
         Me.MaterialRaisedButton1.TabIndex = 0
-        Me.MaterialRaisedButton1.Text = "MaterialRaisedButton1"
+        Me.MaterialRaisedButton1.Text = "?"
         Me.MaterialRaisedButton1.UseVisualStyleBackColor = True
+        '
+        'ToMain
+        '
+        Me.ToMain.Location = New System.Drawing.Point(0, 2)
+        Me.ToMain.Name = "ToMain"
+        Me.ToMain.Size = New System.Drawing.Size(75, 23)
+        Me.ToMain.TabIndex = 1
+        Me.ToMain.Text = "ToMain"
+        Me.ToMain.UseVisualStyleBackColor = True
+        '
+        'Withdraw
+        '
+        Me.Withdraw.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Withdraw.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Withdraw.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Withdraw.Location = New System.Drawing.Point(31, 241)
+        Me.Withdraw.Name = "Withdraw"
+        Me.Withdraw.Size = New System.Drawing.Size(155, 54)
+        Me.Withdraw.TabIndex = 2
+        Me.Withdraw.Text = "Withdraw"
+        Me.Withdraw.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.Withdraw.UseVisualStyleBackColor = False
+        Me.Withdraw.Visible = False
+        '
+        'Deposit
+        '
+        Me.Deposit.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Deposit.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Deposit.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Deposit.Location = New System.Drawing.Point(31, 357)
+        Me.Deposit.Name = "Deposit"
+        Me.Deposit.Size = New System.Drawing.Size(155, 54)
+        Me.Deposit.TabIndex = 3
+        Me.Deposit.Text = "Deposit"
+        Me.Deposit.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.Deposit.UseVisualStyleBackColor = False
+        Me.Deposit.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1403, 644)
+        Me.ClientSize = New System.Drawing.Size(1000, 644)
+        Me.Controls.Add(Me.Deposit)
+        Me.Controls.Add(Me.Withdraw)
+        Me.Controls.Add(Me.ToMain)
         Me.Controls.Add(Me.MaterialRaisedButton1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Welcome to X Bank"
         Me.ResumeLayout(False)
 
     End Sub
 
+
+
     Friend WithEvents MaterialRaisedButton1 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents ToMain As System.Windows.Forms.Button
+    Friend WithEvents Withdraw As System.Windows.Forms.Button
+    Friend WithEvents Deposit As System.Windows.Forms.Button
 End Class
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -9,7 +9,30 @@ Public Class Form1
         SkinManager.ColorScheme = New ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE)
     End Sub
 
+    Private Sub mainPage_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs)
+        ' Determine whether the key entered is the F1 key. Display help if it is.
+        If e.KeyCode = Keys.Q Then
+            ' Display a pop-up help topic to assist the user.
+            MaterialRaisedButton1.Hide()
+        End If
+    End Sub
     Private Sub MaterialRaisedButton1_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton1.Click
         MsgBox("hi")
     End Sub
+
+    Private Sub ToMain_Click(ByVal sender As Object, e As EventArgs) Handles ToMain.Click
+        ToMain.Hide()
+        Deposit.Show()
+        Withdraw.Show()
+        Me.Text = "Welcome User"
+    End Sub
+
+    Private Sub Withdraw_Click(sender As Object, e As EventArgs) Handles Withdraw.Click
+
+    End Sub
+
+    Private Sub Deposit_Click(sender As Object, e As EventArgs) Handles Deposit.Click
+        Throw New NotImplementedException
+    End Sub
+
 End Class
